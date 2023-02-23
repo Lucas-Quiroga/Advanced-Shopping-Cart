dec5aefdb8d624012092942f3ee611f47b37b8f1
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-02-23 20:46:58 -0300

    currency format

--- src/utilities/formatCurrency.ts
+++ src/utilities/formatCurrency.ts
@@ -1,3 +1,9 @@
+/**
+ * El objecto Intl.NumberFormat habilita el formato numérico de acuerdo al lenguaje.
+ */
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {currency:"USD", style:"currency"})
+
+
 export function formatCurrency (number:number) {
-    
+    return CURRENCY_FORMATTER.format(number)
 }
