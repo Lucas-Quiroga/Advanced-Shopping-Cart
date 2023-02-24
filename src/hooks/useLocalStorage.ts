4c669e65eafa0aa965a3d3b5417b86da67f9e46a
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-02-24 19:50:08 -0300

    terminado

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -18,3 +18,15 @@ export function useLocalStorage<T>(key: string, intialValue: T | (()=> T)) {
     
     return [value, setValue] as [typeof value, typeof setValue]
 }
+
+/**
+ * es una función personalizada llamada useLocalStorage que utiliza los hooks useState y useEffect de React.
+
+La función useLocalStorage tiene dos parámetros: key, que es una cadena que se utilizará como clave para almacenar el valor en el almacenamiento local del navegador; e initialValue, que es el valor inicial que se utilizará para la clave proporcionada.
+
+La función devuelve un array que contiene dos elementos: value y setValue. El valor value es el valor actual de la clave proporcionada en el almacenamiento local, y setValue es una función que se puede utilizar para actualizar el valor almacenado.
+
+La función useEffect se utiliza para almacenar el valor actual de la clave proporcionada en el almacenamiento local del navegador siempre que cambie el valor.
+
+En resumen, la función useLocalStorage es útil cuando necesitas almacenar y recuperar datos del almacenamiento local del navegador de una manera fácil y eficiente.
+ */
