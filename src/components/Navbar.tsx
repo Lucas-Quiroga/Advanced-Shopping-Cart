e6789dbb0f42dc09c2fa40e64f20edcb8cab4592
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-06-22 16:53:10 -0300

    retocando proyecto

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-
 import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 
 const Navbar = () => {
@@ -11,15 +10,10 @@ const Navbar = () => {
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
-          <Nav.Link to="/" as={NavLink}>
-            Home
-          </Nav.Link>
-          <Nav.Link to="/store" as={NavLink}>
+          {/* <Nav.Link to="/store" as={NavLink}>
             Store
-          </Nav.Link>
-          <Nav.Link to="/about" as={NavLink}>
-            About
-          </Nav.Link>
+          </Nav.Link> */}
+          <h1>Welcome</h1>
         </Nav>
         {cartQuantity > 0 && (
           <Button
